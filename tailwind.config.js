7201520a98db08a63c5c3104d7330250af9dcef1
hamza <hamzaprogeame@gmail.com>
2024-11-25 11:38:50 +0100

    i created the necessary tailwind envirenment for project building

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './html/**/*.html',          // HTML files in the 'html' folder and its subfolders
+    './assets/js/**/*.js',       // JavaScript files in the 'js' folder and its subfolders
+    './assets/css/**/*.css',     // CSS files in the 'css' folder and its subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
