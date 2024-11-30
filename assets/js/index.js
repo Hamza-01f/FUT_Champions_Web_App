cabcc4a3aca3c0678a0422e33dd7342066c4dca4
hamza <hamzaprogeame@gmail.com>
2024-11-30 20:01:18 +0100

    add the icons of delete and update with their styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,8 @@ let addPlayer = document.getElementById('add-player');
 let addGoalkeeper = document.getElementById('add-goalkeeper');
 let benchPlayers = document.querySelectorAll('.bench');
 let formationCells = document.querySelectorAll('.formation-cell');
-let addBenchPlayer = document.getElementById('players-to-bench');
-let addKeeperToBench = document.getElementById('goalkeepers-bench');
-let PlayerPosition = document.getElementById('player-position')
+
+
 
 let formData = []; // formData array to store player and goalkeeper details
 
@@ -25,7 +24,6 @@ benchPlayers.forEach(bench => {
         } else {
             goalkeeperForm.classList.add('hidden');
             allPlayerForm.classList.remove('hidden');
-            PlayerPosition.classList.add('hidden');
         }
         clickOnCard.classList.add('hidden');
     });
@@ -49,26 +47,9 @@ formationCells.forEach(cell => {
 
         // Hide the card selection
         clickOnCard.classList.add('hidden');
-        PlayerPosition.classList.add('hidden');
     });
 });
 
-//----------------------------[add player or goalkeeper to bench begin ]-------------------------
-addKeeperToBench.addEventListener('click', function(event){
-    event.preventDefault();
-    goalkeeperForm.classList.remove('hidden');
-    clickOnCard.classList.add('hidden');
-    allPlayerForm.classList.add('hidden');
-});
-
-addBenchPlayer.addEventListener('click', function(event){
-    event.preventDefault();
-    allPlayerForm.classList.remove('hidden');
-    PlayerPosition.classList.remove('hidden');
-    goalkeeperForm.classList.add('hidden');
-    clickOnCard.classList.add('hidden');
-
-})
 
 // --------------------- [Add Player Form Submission Logic] --------------------
 addPlayer.addEventListener('click', function(event) {
@@ -196,6 +177,7 @@ addGoalkeeper.addEventListener('click', function(event) {
 
 function createPlayerCard(playerformData, position) {
     const positionCell = document.getElementById(position); // Get the position cell dynamically
+    alert(position)
 
     if (playerformData) {
         const playerCard = document.createElement('div');
@@ -277,8 +259,16 @@ function createPlayerCard(playerformData, position) {
         const buttons = document.createElement('div');
         buttons.classList.add('card-buttons');
         buttons.innerHTML = `
-            <button class="update-btn">Update</button>
-            <button class="delete-btn">Delete</button>
+                        <button class="update-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/>
+                            </svg>
+                        </button>
+                        <button class="delete-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                            </svg>
+                        </button>
         `;
         playerCard.appendChild(buttons);
 
